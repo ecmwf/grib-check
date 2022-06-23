eb0aa12e67fbf316dbd66be3c475c6f80d724885
Eugen Betke <eugen.betke@ecmwf.int>
2022-06-23 13:06:21 +0100

    Fix recursive scan

--- shared.py
+++ shared.py
@@ -6,6 +6,6 @@ import os
 def scan(name, validate):
     if os.path.isdir(name):
         for path in Path(os.path.dirname(name)).rglob('/*'):
-            scan(path)
+            scan(path, validate)
     else:
         validate(name);
