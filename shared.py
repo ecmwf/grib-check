8f637d773f6eb082deb10a1452f55422b29a3569
Eugen Betke <eugen.betke@ecmwf.int>
2022-06-23 12:41:28 +0100

    Oursource scan function; Handle KeyValueNotFoundError in missing()

---
+++ shared.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+import os
+
+def scan(name, validate):
+    if os.path.isdir(name):
+        for path in Path(os.path.dirname(name)).rglob('/*'):
+            scan(path)
+    else:
+        validate(name);
