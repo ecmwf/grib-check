a43bf2ae5e07c17ac95931f53269ff422e32614d
Eugen Betke <eugen.betke@mailbox.org>
2025-03-07 04:56:03 +0100

    Stastistical processing

--- checker/Crra.py
+++ checker/Crra.py
@@ -11,4 +11,4 @@ class Crra(Uerra):
         checks = Report()
         levels = [1000, 975, 950, 925, 900, 875, 850, 825, 800, 750, 700, 600, 500, 400, 300, 250, 200, 150, 100, 70, 50, 30, 20, 10, 7, 5, 3, 2, 1]
         checks.add(IsIn(message, 'level', levels, 'invalid pressure level'))
-        return checks
+        return [checks]
