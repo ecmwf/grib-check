be27015ad2a2ff64ef95d52ff4599ec8724f5d11
Eugen Betke <eugen.betke@ecmwf.int>
2025-05-15 12:42:43 +0200

    Rename destiny to destine

--- checker/DestinE.py
+++ checker/DestinE.py
@@ -4,22 +4,22 @@ from Report import Report
 import logging
 
 
-class Destiny(Wmo):
+class DestinE(Wmo):
     def __init__( self, lookup_table, valueflg=False):
         super().__init__(lookup_table, valueflg=valueflg)
         self.register_checks({
-            "destiny_limits": self._destiny_limits
+            "destine_limits": self._destine_limits
         })
 
     # Reuse / override checks
     def _point_in_time(self, message, p) -> Report:
-        report = Report("Point In Time (Destiny)")
+        report = Report("Point In Time (DestinE)")
         report.add(IsMultipleOf(message["step"], 3))
         return super()._point_in_time(message, p).add(report)
 
     # Create new checks
-    def _destiny_limits(self, message, p) -> Report:
-        report = Report("Destiny Limits")
+    def _destine_limits(self, message, p) -> Report:
+        report = Report("DestinE Limits")
         report.add(Le(message["step"], 30))
         report.add(IsIn(message["forecastTime"], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]))
         report.add(IsIn(message["indicatorOfUnitOfTimeRange"], [0, 1]))
