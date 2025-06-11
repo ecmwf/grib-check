4a57433f63f5849e2586959cc9f03528d78a04e1
Eugen Betke <eugen.betke@ecmwf.int>
2025-06-11 13:44:21 +0200

    Separete Wmo and DestinE parameters

--- GribCheck.py
+++ GribCheck.py
@@ -55,6 +55,7 @@ class GribCheck:
         '''
         script_path = os.path.dirname(os.path.realpath(__file__))
         tigge_params = self.args.parameters if self.args.parameters is not None else f"{script_path}/checker/TiggeParameters.json"
+        destine_params = self.args.parameters if self.args.parameters is not None else f"{script_path}/checker/DestineParameters.json"
         wmo_params = self.args.parameters if self.args.parameters is not None else f"{script_path}/checker/WmoParameters.json"
 
         if self.args.grib_type == "wmo":
@@ -72,7 +73,7 @@ class GribCheck:
         elif self.args.grib_type == "lam":
             checker = Lam(SimpleLookupTable(tigge_params), valueflg=self.args.valueflg)
         elif self.args.grib_type == "destine":
-            checker = DestinE(SimpleLookupTable(wmo_params), valueflg=self.args.valueflg)
+            checker = DestinE(SimpleLookupTable(destine_params), valueflg=self.args.valueflg)
         else:
             raise ValueError("Unknown data type")
 
@@ -98,7 +99,7 @@ if __name__ == "__main__":
     # parser.add_argument("-z", "--zeroflg", help="return 0 to calling shell", action="store_true")
     parser.add_argument("-a", "--valueflg", help="check value ranges", action="store_true")
     parser.add_argument("path", nargs="+", help="path to a GRIB file or directory", type=str)
-    parser.add_argument("-t", "--grib_type", help="type of data to check", choices=["tigge", "s2s", "s2s_refcst", "uerra", "crra", "lam", "wmo", "destine"], default="tigge")
+    parser.add_argument("-t", "--grib_type", help="type of data to check", choices=["tigge", "s2s", "s2s_refcst", "uerra", "crra", "lam", "wmo", "destine"], default="wmo")
     parser.add_argument("-v", "--verbosity", help="increase log verbosity", default=0)
     parser.add_argument("-l", "--report_verbosity", help="report depth", type=int, default=10)
     parser.add_argument("-d", "--debug", help="debug mode", action="store_true")
